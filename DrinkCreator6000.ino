4855ecdd92411726818e2107090bae3656223a48
Kosiarka6000 <alankudelko2000@gmail.com>
2025-06-30 18:33:19 +0200

    Source code update

--- DrinkCreator6000.ino
+++ DrinkCreator6000.ino
@@ -97,17 +97,18 @@ static SemaphoreHandle_t mux_SerialLock{};
 
 // Task variables
 // Stack size will need tuning in last release
-enum{TASK_ERROR_HANDLER_STACK_SIZE=192};   //0
-enum{TASK_STACK_DEBUGGER_STACK_SIZE=192};  //1
-enum{TASK_MAIN_STACK_SIZE=192};            //2
-enum{UPDATE_SCREEN_STACK_SIZE=232};        //3
-enum{TASK_REGULATE_TEMP_STACK_SIZE=128};   //4
-enum{TASK_READ_INPUT_STACK_SIZE=150};      //5
-enum{TASK_SELECT_DRINK_STACK_SIZE=256};    //6
-enum{TASK_ORDER_DRINK_STACK_SIZE=320};     //7
-enum{TASK_SHOW_INFO_STACK_SIZE=256};       //8
-enum{TASK_SHOW_TEMP_STACK_SIZE=192};       //9
-enum{TASK_SHOW_LAST_ERROR_STACK_SIZE=256}; //10
+enum{TASK_ERROR_HANDLER_STACK_SIZE=200};     //0
+enum{TASK_STACK_DEBUGGER_STACK_SIZE=200};    //1
+enum{TASK_MAIN_STACK_SIZE=192};              //2
+enum{UPDATE_SCREEN_STACK_SIZE=232};          //3
+enum{TASK_REGULATE_TEMP_STACK_SIZE=128};     //4
+enum{TASK_READ_INPUT_STACK_SIZE=150};        //5
+enum{TASK_SELECT_DRINK_STACK_SIZE=256};      //6
+enum{TASK_ORDER_DRINK_STACK_SIZE=320};       //7
+enum{TASK_SHOW_INFO_STACK_SIZE=256};         //8
+enum{TASK_SHOW_TEMP_STACK_SIZE=192};         //9
+enum{TASK_SHOW_LAST_ERROR_STACK_SIZE=256};   //10
+enum{TASK_KEYBOARD_SIM_STACK_SIZE=192};      //11
 
 enum{TASK_ERROR_HANDLER=0,
 	 TASK_STACK_DEBUGGER=1,
@@ -119,11 +120,13 @@ enum{TASK_ERROR_HANDLER=0,
 	 TASK_ORDER_DRINK=7,
 	 TASK_SHOW_INFO=8,
 	 TASK_SHOW_TEMP=9,
-	 TASK_SHOW_LAST_ERROR=10};
+	 TASK_SHOW_LAST_ERROR=10,
+   TASK_KEYBOARD_SIM=11
+};
 
-enum{GUARD_ZONE_SIZE=32};
+enum{GUARD_ZONE_SIZE=16};
 	 
-enum{TASK_N=11}; // Zmiana
+enum{TASK_N=12}; // Zmiana
 
 static StaticTask_t errorHandlerTCB{0xF};                                   //0
 static StaticTask_t stackDebuggerTCB{0xF};                                  //1
@@ -136,6 +139,7 @@ static StaticTask_t orderDrinkTCB{0xF};                                     //7
 static StaticTask_t showInfoTCB{0xF};                                       //8
 static StaticTask_t showTempTCB{0xF};                                       //9
 static StaticTask_t showLastErrorTCB{0xF};                                  //10
+static StaticTask_t keyboardSimTCB{0xF};                                    //11
 
 static TaskHandle_t taskHandles[TASK_N]{0xF};
 
@@ -161,6 +165,8 @@ static StackType_t showTempStack[TASK_SHOW_TEMP_STACK_SIZE]{0xF};           //9
 static StackType_t guardZone9[GUARD_ZONE_SIZE]{0xF};
 static StackType_t showLastErrorStack[TASK_SHOW_LAST_ERROR_STACK_SIZE]{0xF};//10
 static StackType_t guardZone10[GUARD_ZONE_SIZE]{0xF};
+static StackType_t keyboardSimStack[TASK_KEYBOARD_SIM_STACK_SIZE]{0xF};     //11
+static StackType_t guardZone11[GUARD_ZONE_SIZE]{0xF};
 // Add guardzones between variables for potential safe overwrite
 // Task variables
 
@@ -197,6 +203,7 @@ void initializeMemory(){
   memset(guardZone8,0xF,GUARD_ZONE_SIZE);
   memset(guardZone9,0xF,GUARD_ZONE_SIZE);
   memset(guardZone10,0xF,GUARD_ZONE_SIZE);
+  memset(guardZone11,0xF,GUARD_ZONE_SIZE);
 }
 void ram_dump(){
   char buffer[6]{};
@@ -318,6 +325,7 @@ void taskErrorHandler(void*pvParameters){
 	    Serial.println(F("[####################]====SYSTEM CRITICAL====[##]"));
       Serial.println((const char*)lastError.errorText);
       Serial.println(F("[####################]====SYSTEM CRITICAL====[##]"));
+      Serial.println("");
       EEPROMUpdateLastStartupError(&lastError);
       f_run=false;
     }
@@ -728,6 +736,11 @@ void taskShowLastError(void*pvParameters){
     vTaskDelay(pdMS_TO_TICKS(300));
   }
 }
+void taskKeyboardSim(void*pvParameters){
+  for(;;){
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
 void setup(){
   while(!eeprom_is_ready());
   
@@ -745,48 +758,6 @@ void setup(){
   lcd.load_custom_character(5,fullSquare);
   lcd.load_custom_character(6,fullSquare);
   lcd.load_custom_character(7,fullSquare);
-
-// Set IOCON: BANK=1, MIRROR=0, SEQOP=1 (disable auto increment)
-Wire.beginTransmission(0x20);
-Wire.write(0x0A);    // IOCON address in BANK=1
-Wire.write(0xA0);    // 0b10100000 → BANK=1, SEQOP=1
-Wire.endTransmission();
-
-// IODIRA: all GPA pins as input
-Wire.beginTransmission(0x20);
-Wire.write(0x00);    // IODIRA
-Wire.write(0xFF);
-Wire.endTransmission();
-
-// IPOLA: no inversion
-Wire.beginTransmission(0x20);
-Wire.write(0x01);    // IPOLA
-Wire.write(0x00);    // No polarity inversion
-Wire.endTransmission();
-
-// GPINTENA: enable interrupt on all GPA pins
-Wire.beginTransmission(0x20);
-Wire.write(0x02);    // GPINTENA
-Wire.write(0xFF);
-Wire.endTransmission();
-
-// DEFVALA: default value = 1 (high)
-Wire.beginTransmission(0x20);
-Wire.write(0x03);    // DEFVALA
-Wire.write(0xFF);
-Wire.endTransmission();
-
-// INTCONA: compare against DEFVAL
-Wire.beginTransmission(0x20);
-Wire.write(0x04);    // INTCONA
-Wire.write(0xFF);
-Wire.endTransmission();
-
-// GPPUA: enable pull-ups on all GPA pins
-Wire.beginTransmission(0x20);
-Wire.write(0x06);    // GPPUA
-Wire.write(0xFF);
-Wire.endTransmission();  
   
   Serial.begin(9600);
   
@@ -813,6 +784,8 @@ Wire.endTransmission();
   taskHandles[TASK_SHOW_INFO]      =xTaskCreateStatic(taskShowInfo     ,"SHOW INFO"    ,TASK_SHOW_INFO_STACK_SIZE      ,(void*)&bootupsCount    ,1,showInfoStack,&showInfoTCB);
   taskHandles[TASK_SHOW_TEMP]      =xTaskCreateStatic(taskShowTemp     ,"SHOW TEMP"    ,TASK_SHOW_TEMP_STACK_SIZE      ,NULL                    ,1,showTempStack,&showTempTCB);
   taskHandles[TASK_SHOW_LAST_ERROR]=xTaskCreateStatic(taskShowLastError,"LAST ERROR"   ,TASK_SHOW_LAST_ERROR_STACK_SIZE,(void*)&f_errorConfirmed,2,showLastErrorStack,&showLastErrorTCB);
+  taskHandles[TASK_KEYBOARD_SIM]   =xTaskCreateStatic(taskKeyboardSim  ,"KEYBOARD SIM" ,TASK_KEYBOARD_SIM_STACK_SIZE   ,NULL                    ,1,keyboardSimStack,&keyboardSimTCB);
+
 }
 void loop(){
 
