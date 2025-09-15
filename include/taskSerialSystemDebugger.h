99c5a96e2f18bc72686076531236ea79fcef59e8
Kosiarka6000 <alankudelko2000@gmail.com>
2025-09-15 16:57:16 +0200

    Convert taskSerialDebugger.* to be MISRA C:2025 Compliant

--- include/taskSerialSystemDebugger.h
+++ include/taskSerialSystemDebugger.h
@@ -1,8 +1,30 @@
-#ifndef _TASK_SERIAL_SYSTEM_DEBUGGER_H_
-    #define _TASK_SERIAL_SYSTEM_DEBUGGER_H_
+// Doxy
 
-extern "C" void updateMemoryUsage();
-extern "C" void ram_dump();
+#ifndef TASK_SERIAL_SYSTEM_DEBUGGER_H_
+    #define TASK_SERIAL_SYSTEM_DEBUGGER_H_
+
+#define RAM_DUMP_BUFFER_LENGTH 5
+#define RAM_DUMP_ADDR_FMT      "%04X"
+#define RAM_DUMP_SIZE_FMT      "%4d"
+
+#ifdef __cplusplus
+    extern "C"{
+#endif // __cplusplus
+
+void updateMemoryUsage(void);
+
+/**
+ * @brief   Dumps memory usage via UART.
+ * @details Prints boundaries and sizes of .data, .bss, .tdat, heap, and stack.
+ * @note    Uses blocking UART output.
+ */
+void ram_dump(void);
+
+
 void taskSerialSystemDebugger(void*pvParameters);
 
-#endif // _TASK_SERIAL_SYSTEM_DEBUGGER_H_
+#ifdef __cplusplus
+    }
+#endif // __cplusplus
+
+#endif // TASK_SERIAL_SYSTEM_DEBUGGER_H_
--- src/taskSerialSystemDebugger.c
+++ src/taskSerialSystemDebugger.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #include <taskSerialSystemDebugger.h>
 #include <uart.h>
@@ -7,51 +8,66 @@
 
 #include <DrinkCreator6000_RamStats.h>
 
-void ram_dump(){
-  // Convert code to be MISRA C 2025 compliant
-  char buffer[5]={0};
-  updateMemoryUsage();
-  
-  uart_puts_P_blocking(msg_ramDump_header1);
-  uart_puts_P_blocking(msg_ramDump_header2);
+void ram_dump(void){
+    /* buffer is initialized with NUL characters to avoid undefined content
+      * Rule: MISRA C 2025 Dir 9.2 – all automatic variables must be initialized */
+    char buffer[RAM_DUMP_BUFFER_LENGTH]={'\0'};
+    /* Variable to store return value of snprintf
+     * MISRA C 2025 17.x – return value of functions that may fail must be checked */
+    int32_t snprintf_ret_val=0;
 
-  snprintf(buffer,sizeof(buffer),"%04X",(uint16_t)&__data_start);
-  uart_puts_P_blocking(msg_ramDump_data_header); uart_puts_blocking(buffer);
-  snprintf(buffer,sizeof(buffer),"%04X",(uint16_t)&__data_end);
-  uart_puts_P_blocking(msg_ramDump_addr_header); uart_puts_blocking(buffer);
-  snprintf(buffer,sizeof(buffer),"%4d",(uint16_t)((uint16_t)&__data_end-(uint16_t)&__data_start));
-  uart_puts_P_blocking(msg_ramDump_seperator); uart_puts_blocking(buffer); uart_puts_P_blocking(msg_ramDump_data_footer);
+    updateMemoryUsage();
 
-  snprintf(buffer,sizeof(buffer),"%04X",(uint16_t)&__bss_start);
+    /* Output headers stored in PROGMEM (MISRA 8.13 – use const for read-only data) */
+    uart_puts_P_blocking(msg_ramDump_header1);
+    uart_puts_P_blocking(msg_ramDump_header2);
+
+    snprintf_ret_val=snprintf(buffer,sizeof(buffer),RAM_DUMP_ADDR_FMT,(unsigned int)(uintptr_t)&__data_start);
+    if((snprintf_ret_val<0)||(snprintf_ret_val>=(int32_t)sizeof(buffer))){
+        buffer[0]='\0';
+    }
+    uart_puts_P_blocking(msg_ramDump_data_header); 
+    uart_puts_blocking(buffer);
+
+    snprintf(buffer,sizeof(buffer),RAM_DUMP_ADDR_FMT,(uint16_t)&__data_end);
+    uart_puts_P_blocking(msg_ramDump_addr_header);
+    uart_puts_blocking(buffer);
+
+    snprintf(buffer,sizeof(buffer),RAM_DUMP_SIZE_FMT,(uint16_t)((uint16_t)&__data_end-(uint16_t)&__data_start));
+    uart_puts_P_blocking(msg_ramDump_seperator);
+    uart_puts_blocking(buffer);
+    uart_puts_P_blocking(msg_ramDump_data_footer);
+
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_ADDR_FMT,(uint16_t)&__bss_start);
   uart_puts_P_blocking(msg_ramDump_bss_header); uart_puts_blocking(buffer);
-  snprintf(buffer,sizeof(buffer),"%04X",(uint16_t)&__bss_end);
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_ADDR_FMT,(uint16_t)&__bss_end);
   uart_puts_P_blocking(msg_ramDump_addr_header); uart_puts_blocking(buffer);
-  snprintf(buffer,sizeof(buffer),"%4d",(uint16_t)((uint16_t)&__bss_end-(uint16_t)&__bss_start));
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_SIZE_FMT,(uint16_t)((uint16_t)&__bss_end-(uint16_t)&__bss_start));
   uart_puts_P_blocking(msg_ramDump_seperator); uart_puts_blocking(buffer); uart_puts_P_blocking(msg_ramDump_bss_footer);
 
-  snprintf(buffer,sizeof(buffer),"%04X",(uint16_t)&__tdat_start);
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_ADDR_FMT,(uint16_t)&__tdat_start);
   uart_puts_P_blocking(msg_ramDump_tdat_header); uart_puts_blocking(buffer);
-  snprintf(buffer,sizeof(buffer),"%04X",(uint16_t)&__tdat_end);
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_ADDR_FMT,(uint16_t)&__tdat_end);
   uart_puts_P_blocking(msg_ramDump_addr_header); uart_puts_blocking(buffer);
-  snprintf(buffer,sizeof(buffer),"%4d",(uint16_t)((uint16_t)&__tdat_end-(uint16_t)&__tdat_start));
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_SIZE_FMT,(uint16_t)((uint16_t)&__tdat_end-(uint16_t)&__tdat_start));
   uart_puts_P_blocking(msg_ramDump_seperator); uart_puts_blocking(buffer); uart_puts_P_blocking(msg_ramDump_tdat_footer);  
 
-  snprintf(buffer,sizeof(buffer),"%04X",(uint16_t)&__heap_start);
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_ADDR_FMT,(uint16_t)&__heap_start);
   uart_puts_P_blocking(msg_ramDump_heap_header); uart_puts_blocking(buffer);
-  snprintf(buffer,sizeof(buffer),"%04X",(uint16_t)__heap_end);
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_ADDR_FMT,(uint16_t)__heap_end);
   uart_puts_P_blocking(msg_ramDump_addr_header); uart_puts_blocking(buffer);
-  snprintf(buffer,sizeof(buffer),"%4d",__heap_size);
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_SIZE_FMT,__heap_size);
   uart_puts_P_blocking(msg_ramDump_seperator); uart_puts_blocking(buffer); uart_puts_P_blocking(msg_ramDump_heap_footer);
 
-  snprintf(buffer,sizeof(buffer),"%04X",(uint16_t)__stack_ptr);
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_ADDR_FMT,(uint16_t)__stack_ptr);
   uart_puts_P_blocking(msg_ramDump_stack_header); uart_puts_blocking(buffer);
-  snprintf(buffer,sizeof(buffer),"%04X",RAMEND);
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_ADDR_FMT,RAMEND);
   uart_puts_P_blocking(msg_ramDump_addr_header); uart_puts_blocking(buffer);
-  snprintf(buffer,sizeof(buffer),"%4d",__stack_size);
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_SIZE_FMT,__stack_size);
   uart_puts_P_blocking(msg_ramDump_seperator); uart_puts_blocking(buffer); uart_puts_P_blocking(msg_ramDump_stack_footer);
 
   uart_puts_P_blocking(msg_ramDump_free_header);
-  snprintf(buffer,sizeof(buffer),"%4d",ram_total_free);
+  snprintf(buffer,sizeof(buffer),RAM_DUMP_SIZE_FMT,ram_total_free);
   uart_puts_blocking(buffer);
   uart_puts_P_blocking(msg_ramDump_free_footer);
 
@@ -60,15 +76,15 @@ void ram_dump(){
 void taskSerialSystemDebugger(void*pvParameters){
   char buffer[configMAX_TASK_NAME_LEN+1]={0};
   uint8_t i=0;
-  uint8_t nameLength{};
-  TaskStatus_t taskStatus{};
+  uint8_t nameLength={0};
+  TaskStatus_t taskStatus={0};
   for(;;){
       uart_putc_blocking('\n');
       uart_puts_P_blocking(msg_serialSystemDebugger_header1);
       uart_puts_P_blocking(msg_serialSystemDebugger_header2);
       for(i=0;i<TASK_N;i++){
         memset((void*)buffer,0,sizeof(buffer));
-        if(taskHandles[i]==nullptr)
+        if(taskHandles[i]==NULL)
           continue;
         vTaskGetInfo(taskHandles[i],&taskStatus,pdTRUE,eInvalid);
         sprintf(buffer,"%s",taskStatus.pcTaskName);
